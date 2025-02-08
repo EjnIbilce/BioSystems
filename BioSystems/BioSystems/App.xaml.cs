b0a9408fa2c7474ab9065952235437fbbf505b1d
RenanSpim <renan.sinhorini@unesp.br>
2025-02-08 17:13:02 -0300

    criado o basico

---
+++ BioSystems/BioSystems/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BioSystems
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
