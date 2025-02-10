f58a5e5932f6481df321f321e6002dfa6a2330b2
RenanSpim <renan.sinhorini@unesp.br>
2025-02-10 12:00:12 -0300

    paginas de login e cadastro adicionadas (sem CSS)

---
+++ BioSystems/BioSystems/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioSystems {
+    public partial class LoginPage : ContentPage {
+        public LoginPage() {
+            InitializeComponent();
+        }
+
+        private async void onLoginClicked(Object sender, EventArgs e) {
+            await DisplayAlert("Login", "Butao de login clicado", "Ok");
+        }
+
+        private async void onRegisterClicked(Object sender, EventArgs e) {
+            await Shell.Current.GoToAsync("RegisterPage");
+        }
+    }
+}
---
+++ BioSystems/BioSystems/RegisterPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioSystems {
+    public partial class RegisterPage : ContentPage {
+        public RegisterPage() {
+            InitializeComponent();
+        }
+
+        private async void onRegisterClicked(Object sender, EventArgs e) {
+            await DisplayAlert("Register", "registro clicado", "oki doki");
+        }
+
+        private async void onLoginClicked(Object sender, EventArgs e) {
+            await Shell.Current.GoToAsync("LoginPage");
+        }
+    }
+}
